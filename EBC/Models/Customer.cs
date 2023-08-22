0ef3cdaa1ed08700257fa72ac69db139c7bfc086
AlBaraa-mohamed <albaraamohamedmahmoud@gmail.com>
2023-08-22 16:20:13 +0300

    first commit

---
+++ EBC/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EBC;
+
+public partial class Customer
+{
+    public long CustomerId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+}
